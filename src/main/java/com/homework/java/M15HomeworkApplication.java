d3d793e117520b71368ce09352aefd9693782779
ZabeidaNazar <nazarzabeida@gmail.com>
2024-01-07 22:41:50 +0200

    Initial commit

---
+++ src/main/java/com/homework/java/M15HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M15HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M15HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/homework/java/data/entity/Note.java
@@ -0,0 +1,30 @@
+package com.homework.java.data.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
+    @Size(min = 1, max = 200)
+    private String title;
+
+    @Column(nullable = false)
+    @Size(min = 5)
+    private String content;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
